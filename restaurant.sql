13c533c8c7efe6a4f1f360b04aa1647503e026a1
grayyeargin <grayyeargin@gmail.com>
2014-10-14 12:22:02 -0400

    Create database and tables in sql doc

---
+++ restaurant.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS restaurant;
+
+CREATE DATABASE restaurant;
+
+CREATE TABLE food(
+id serial PRIMARY KEY,
+name varchar(100),
+price integer,
+spicy boolean
+);
+
+CREATE TABLE parties(
+id serial PRIMARY KEY,
+table_number integer,
+guests integer,
+payment_status boolean
+);
+
+CREATE TABLE orders(
+	id serial PRIMARY KEY,
+	food_id integer,
+	party_id integer,
+	time TIMESTAMP
+);
