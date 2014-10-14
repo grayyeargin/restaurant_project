d05429f0450f5aded9aacf8f3d0d28b8a7852be3
grayyeargin <grayyeargin@gmail.com>
2014-10-14 13:46:22 -0400

    Create CRUD routes for each model

--- app.rb
+++ app.rb
@@ -6,6 +6,114 @@ ActiveRecord::Base.establish_connection(
 	database: "restaurant"
 	)
 
+
+# Routes for foods
+
 get '/' do 
-	"HELLO WORLD"
+	redirect "/foods"
 end
+
+get '/foods' do 
+
+end
+
+get '/foods/new' do 
+
+end
+
+post '/foods' do 
+
+end
+
+get '/foods/:id' do 
+
+end 
+
+get '/foods/:id/edit' do
+
+end
+
+patch '/foods/:id' do 
+
+end
+
+delete '/foods/:id' do 
+
+end
+
+# Routes for parties
+
+get '/parties' do 
+
+end
+
+get '/parties/new' do 
+
+end
+
+post '/parties' do 
+
+end
+
+get '/parties/:id' do 
+
+end
+
+get '/parties/:id/edit' do 
+
+end
+
+patch '/parties/:id' do 
+
+end
+
+delete '/parties/:id' do 
+
+end
+
+# Create Orders
+post '/orders' do 
+
+end
+
+patch '/orders/:id' do 
+
+end
+
+delete '/orders' do 
+
+end
+
+get '/parties/:id/receipt' do 
+
+end
+
+patch '/parties/:id/checkout' do 
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
